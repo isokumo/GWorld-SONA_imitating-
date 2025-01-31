5be6f84f5869d91e8bc1cbf9a7d0c8ce6590d15b
sso <isokumo@naver.com>
2025-01-31 17:46:49 +0900

    slider text -ing

--- js/main.js
+++ js/main.js
@@ -38,11 +38,49 @@ $(document).ready(function(){
         if($('.m-search').hasClass('input-wide')){
             $('.m-search').attr('placeholder',"");
         }else{
-            $('.m-search').attr('placeholder','검색...');
+            $('.m-search').attr('placeholder','검색...').focus();
         }
         $('.m-search').toggleClass('input-wide');
     });
-     
+
+    //접속 상태를 확인하는 두 가지 방법
+    /*
+     1. media (브라우저 화면 크기를 확인)
+        if(window.matchMedia("(max-width: 768px)").matches){
+          $('.col-nav').click(function(){
+         $(this).find(".nav").stop().slideToggle(300);    
+         });       
+        }
+        >> 이렇게 하면 처음 켜진 상태에서 한 번만 읽기 때문에
+            켜진 후에는 화면크기를 바꿔도 적용이 안 됨!
+      
+     2. 접속한 디바이스의 종류 (navigator.userAgent)
+      
+        if(/Mobi|Android|iPone|iPad|iPod/i.test(navigator.userAgent)){
+         $('.col-nav').click(function(){
+         $(this).find(".nav").stop().slideToggle(300);    
+         });
+        }
+        >> 이렇게 하면 지금 웹브라우저에서는 모바일 상태를 못 봄!
+      
+     */
+
+    $('.col-nav').click(function(){
+        $(this).find(".nav").stop().slideToggle(300);    
+    });
+    
+
+
+    /********slick*********/
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '30px',
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
 });
 
 function slideUp(){
