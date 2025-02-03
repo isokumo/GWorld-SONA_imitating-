b66c8e92115f296716df9c81cc3d6769ed9ccb7d
sso <isokumo@naver.com>
2025-02-03 17:25:13 +0900

    slider complete

--- js/main.js
+++ js/main.js
@@ -69,17 +69,94 @@ $(document).ready(function(){
         $(this).find(".nav").stop().slideToggle(300);    
     });
     
+    // const imgs = ["001.jpg","002.jpg","003.jpg","004.jpg","005.jpg","006.jpg",
+    //              "007.jpg","008.jpg","009.jpg","010.jpg","011.jpg","012.jpg",
+    //              "013.jpg","014.jpg","015.jpg","016.jpg","017.jpg","018.jpg"]
+    // let tags = "";
+    // let nums = 0;
+
+    // for(let i =0; i <=5; i++){
+    //     tags += `<div class = "box">`;
+    //     for(let n=0; n <=2; n++){
+    //         tags += `<div class = "fadebox">
+    //                     <img src="images/pd/${imgs[nums]}" alt="${imgs[nums]}">
+    //                     <div class="text-box">
+    //                         <div class="btn">WIDE SLACKS</div>
+    //                         <h1>[MADE IN KOREA]</h1>
+    //                         <p>4가지 컬러, 2가지 디자인, 3가지 기장으로 준비했지만 품절!</p>
+    //                     </div>
+    //                 </div>`;
+    //         nums++;
+    //     }
+    //     tags += `</div>`;
+    // }
+
+    // $('.slider').html(tags);
+
+
+
+    $.getJSON("js/data.json", (data)=>{
+        let tags = "";
+
+        //json 데이터를 이용하여 루프를 돌며 html을 생성
+        $.each(data, (index,item)=>{
+            tags += `<div class = "box">`;
+            $.each(item.images,(i, img)=>{
+                tags += `<div class = "fadebox">
+                            <img src = "images/pd/${img}" alt="${img}">
+                            <div class = "text-box">
+                                <div class = "btn">${item.category}</div>
+                                <h1>${item.origin}</h1>
+                                <p>${item.desc}</p>
+                            </div>
+                        </div>`;
+            });
+            tags += `</div>`;
+        });
+
+        //동적으로 생성한 html을 .slider애 추가
+        $('.slider').html(tags);
+
+        //slick 초기화 (생성한 html을 slick에 담기)
+        initSlick();
+    })
+
+    function initSlick(){
+
+    let slider = $(".slider");
+    let innerSlider = $(".box");
+
+
 
 
     /********slick*********/
-    $('.slider').slick({
+    slider.slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         centerMode: true,
         centerPadding: '30px',
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 6000,
+        dots:true,
+        responsive: [
+            {
+             breakpoint: 600, 
+             slidesToShow: 1,
+             slideToScroll: 1
+            }
+        ]
     });
+
+    innerSlider.slick({
+        speed:600,
+        fade:true,
+        infinite:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+       dots : false,
+       arrows : false
+    });
+   }
 
 });
 
